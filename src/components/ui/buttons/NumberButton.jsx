9a3b39fcf106f0465bdaac7a95e2c49bcd645e86
Carlos de Leon <admin@koxland.net>
2025-06-02 16:04:06 -0600

    consoleLog Value

--- src/components/ui/buttons/NumberButton.jsx
+++ src/components/ui/buttons/NumberButton.jsx
@@ -1,13 +1,14 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { ThemeContext } from "../../../context/ThemeContext";
 import themes from "../../shared/themes";
 
-function NumberButton({ text = "" }) {
+function NumberButton({ text = "", onClick }) {
   const { theme } = useContext(ThemeContext);
   const currentTheme = themes.find((t) => t.name === theme) || themes[0];
 
   return (
     <button
+      onClick={() => onClick(text)}
       className={`flex items-center justify-center w-[60px] h-[60px] sm:w-[101px] rounded-[5px] text-3xl font-bold 
         ${currentTheme.bgButton} ${currentTheme.text} ${currentTheme.hover} ${currentTheme.shadowButton} cursor-pointer`}
     >
--- src/components/ui/containers/MainContainer.jsx
+++ src/components/ui/containers/MainContainer.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
-import HeaderContainer from './HeaderContainer'
-import ButtonsContainer from './ButtonsContainer'
-import CalculationContainer from './CalculationContainer'
+import React, { useState } from "react";
+import HeaderContainer from "./HeaderContainer";
+import ButtonsContainer from "./ButtonsContainer";
+import CalculationContainer from "./CalculationContainer";
 
 function MainContainer() {
+  const [calculation, setCalculation] = useState("0");
+  const handleButtonClick = (value) => {
+    console.log("Boton:", value);
+  };
   return (
-    <div className='flex flex-col '>
-        <HeaderContainer />
-        <CalculationContainer />
-        <ButtonsContainer />
+    <div className="flex flex-col">
+      <HeaderContainer />
+      <CalculationContainer />
+      <ButtonsContainer onButtonClick={handleButtonClick} />
     </div>
-  )
+  );
 }
 
-export default MainContainer
+export default MainContainer;
