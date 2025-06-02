a1f133327a20325b2f230bdd8d25ff83f2a25e44
Carlos de Leon <admin@koxland.net>
2025-06-02 16:51:51 -0600

    logic

--- src/components/ui/containers/CalculationContainer.jsx
+++ src/components/ui/containers/CalculationContainer.jsx
@@ -2,15 +2,15 @@ import React, { useContext } from "react";
 import { ThemeContext } from "../../../context/ThemeContext";
 import themes from "../../shared/themes";
 
-function CalculationContainer({ calculation = '0'}) {
+function CalculationContainer({ calculation = "0" }) {
   const { theme } = useContext(ThemeContext);
   const currentTheme = themes.find((t) => t.name === theme) || themes[0];
   return (
     <div
       className={`flex items-center justify-end text-[40px] sm:text-[56px] text-right 
-    py-7 px-6 sm:py-10 sm:px-8 rounded-xl h-[88px] sm:h-[128px] font-bold tracking-[-0.6704px] ${currentTheme.calculationText} ${currentTheme.calculationBg}`}
+    py-7 px-6 sm:py-10 sm:px-8 max-w-[327px] sm:max-w-[540px] overflow-hidden rounded-xl h-[88px] sm:h-[128px] font-bold tracking-[-0.6704px] ${currentTheme.calculationText} ${currentTheme.calculationBg}`}
     >
-      {calculation}
+      {calculation.slice(0, 13)}
     </div>
   );
 }
--- src/components/ui/containers/MainContainer.jsx
+++ src/components/ui/containers/MainContainer.jsx
@@ -6,12 +6,51 @@ import CalculationContainer from "./CalculationContainer";
 function MainContainer() {
   const [calculation, setCalculation] = useState("0");
   const handleButtonClick = (value) => {
-    console.log("Boton:", value);
+
+    if (value === "reset") {
+      setCalculation("0");
+      return;
+    }
+
+    if (calculation === "Error") {
+      setCalculation("0");
+    }
+
+    if (value === "del") {
+      if (calculation.length <= 1 || calculation === "0") {
+        setCalculation("0");
+      } else {
+        setCalculation(calculation.slice(0, -1)); // Elimina el último carácter, -2 elimina ultimos 2 y asi sucesivamente
+      }
+      return;
+    }
+
+    if (value === "=") {
+      try {
+        const expresion = calculation.replace(/x/g, "*"); //Si el valor es =, toma lo que hay en calculation y reemplaza x por *
+        const resultado = eval(expresion); //expresion es la cadena de texto que contiene lo que el usuario ha escrito, eval convierte el string en una expresión matemática
+        setCalculation(resultado.toString()); // Convierte el resultado a string para que se pueda mostrar en la pantalla
+      } catch {
+        setCalculation("Error");
+      }
+      return;
+    }
+
+    if (calculation.length >= 13) {
+      return;
+    }
+
+    if (calculation === "0") {
+      setCalculation(value);
+    } else {
+      const nuevaCadena = calculation + value;
+      setCalculation(nuevaCadena);
+    }
   };
   return (
     <div className="flex flex-col">
       <HeaderContainer />
-      <CalculationContainer />
+      <CalculationContainer calculation={calculation} />
       <ButtonsContainer onButtonClick={handleButtonClick} />
     </div>
   );
