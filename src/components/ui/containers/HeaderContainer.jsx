645e82e643d84e65b699fc02da067d267b54554b
Julie Iskander <34782134+jIskCoder@users.noreply.github.com>
2025-06-04 09:27:53 +1000

    add header

--- src/components/ui/containers/HeaderContainer.jsx
+++ src/components/ui/containers/HeaderContainer.jsx
@@ -5,6 +5,7 @@ import ToggleThemeButton from '../buttons/ToggleThemeButton'
 function HeaderContainer() {
   return (
     <div className='flex items-center justify-between mb-8'>
+      <h1>Hello</h1>
         <MainLogo />
         <ToggleThemeButton />
     </div>
